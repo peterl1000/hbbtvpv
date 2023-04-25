9f9c8b73c09d1623ed3511fbdac4659628b0e2a2
PL <a@b>
2023-04-25 14:10:07 +0200

    Implemented swapping between JSLIB and video element for playback

--- hbbtvpv.js
+++ hbbtvpv.js
@@ -3,16 +3,20 @@
 // Otherwise a default video is used
 
 const StreamType = {
-    "DASH" : 0,
-    "HLS": 1,
-    "VE": 2     // Playback directly in the Video element
+    "DASH": 0,
+    "HLS": 1
+}
+const PlaybackType = {
+    "JSLIB": 0,   // Playback using Javascript library - dash.js or hls.js
+    "VE": 1     // Playback directly in the Video element
 }
 let streamtype = undefined;
-
+let playbacktype = undefined;
 let videourl = undefined;
 
 const DEFAULTVIDEOURL = "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd";
 const DEFAULTSTREAMTYPE = "DASH";
+const DEFAULTPLAYBACKTYPE = "JSLIB";
 let player;
 const VideoState = {
     STOPPED: 0,
@@ -26,6 +30,8 @@ function hbbtvpv_init() {
     if(videourl === undefined) {
         videourl = DEFAULTVIDEOURL;
     }
+    console.log("Video URL: " + videourl);
+
     let streamtypefromurl = queryURLParameter("t");
     if(streamtypefromurl in StreamType) {
         streamtype = StreamType[streamtypefromurl];
@@ -33,13 +39,18 @@ function hbbtvpv_init() {
         // Test if the stream type looks like HLS...
         if(videourl.endsWith(".m3u") || videourl.endsWith(".m3u8")) {
             console.log("Guessing this is an HLS stream from the URL ending");
-            streamtype = StreamType["VE"];
+            streamtype = StreamType["HLS"];
         } else {
             streamtype = StreamType[DEFAULTSTREAMTYPE];
         }
     }
 
-
+    let playbacktypefromurl = queryURLParameter("p");
+    if(playbacktypefromurl in PlaybackType) {
+        playbacktype = PlaybackType[playbacktypefromurl];
+    } else {
+        playbacktype = PlaybackType[DEFAULTPLAYBACKTYPE];
+    }
 
     try {
         // create the media player - this is needed whether the app runs in an HbbTV
@@ -56,9 +67,12 @@ function hbbtvpv_init() {
         } 
         else {
             // Register the keyset
-            const MASK_CONSTANT_RED = 0x1
+            const MASK_CONSTANT_RED = 0x1;
+            const MASK_CONSTANT_GREEN = 0x2;
             const MASK_CONSTANT_BLUE = 0x8;
-            appObject.privateData.keyset.setValue(MASK_CONSTANT_RED + MASK_CONSTANT_BLUE);
+            appObject.privateData.keyset.setValue(MASK_CONSTANT_RED +
+                                                  MASK_CONSTANT_BLUE +
+                                                  MASK_CONSTANT_GREEN);
 
             // we have the Application object, and we can show our app
             appObject.show();
@@ -86,6 +100,11 @@ function keyDown(event) {
         case 406: // hopefully this is "blue"
             toggleTextbox();
             toggleAVInfo();
+            break;
+
+        case 71: // g
+        case 404: // hopefully this is "green"
+            swapPlaybackType();
             break;
 
         default:
@@ -121,65 +140,72 @@ function startVideo() {
 function initMediaPlayer(type) {
     let rplayer = undefined;
 
-    switch(type) {
-        case StreamType.DASH:
-            console.log("Stream type: MPEG-DASH");
-            rplayer = dashjs_initMediaPlayer();
-            break;
+    if(playbacktype === PlaybackType.JSLIB) {
+        // JSLIB playback
+        switch(type) {
+            case StreamType.DASH:
+                console.log("Stream type: MPEG-DASH");
+                rplayer = dashjs_initMediaPlayer();
+                break;
 
-        case StreamType.HLS:
-            console.log("Stream type: HLS");
-            rplayer = hlsjs_initMediaPlayer();
-            break;
-
-        case StreamType.VE:
-            console.log("Stream type: Video element");
-            rplayer = ve_initMediaPlayer();
-            break;
+            case StreamType.HLS:
+                console.log("Stream type: HLS");
+                rplayer = hlsjs_initMediaPlayer();
+                break;
         
-        default:
-            console.log("Stream type: unknown");
-            break;
+            default:
+                console.log("Stream type: unknown");
+                break;
+        }
+    } else if(playbacktype === PlaybackType.VE) {
+        console.log("Stream type: Video element");
+        rplayer = ve_initMediaPlayer();
+    } else {
+        console.log("Unknown playback type");
     }
 
     return rplayer;
 }
 
 function startMediaPlayer(player, videoelement, url, servicetype, ttmlrenderingdiv = undefined) {
-    switch(streamtype) {
-        case StreamType.DASH:
-            dashjs_startDashMediaPlayer(player, videoelement, url, servicetype, ttmlrenderingdiv);
-            break;
-        
-        case StreamType.HLS:
-            hlsjs_startMediaPlayer(player, videoelement, url, servicetype, ttmlrenderingdiv);
-            break;
+    if(playbacktype === PlaybackType.JSLIB) {
+        switch(streamtype) {
+            case StreamType.DASH:
+                dashjs_startDashMediaPlayer(player, videoelement, url, servicetype, ttmlrenderingdiv);
+                break;
+            
+            case StreamType.HLS:
+                hlsjs_startMediaPlayer(player, videoelement, url, servicetype, ttmlrenderingdiv);
+                break;
 
-        case StreamType.VE:
-            rplayer = ve_startMediaPlayer(player, videoelement, url, servicetype, ttmlrenderingdiv);
-            break;
-
-        default:
-            break;
+            default:
+                break;
+        }
+    } else if(playbacktype === PlaybackType.VE) {
+        ve_startMediaPlayer(player, videoelement, url, servicetype, ttmlrenderingdiv);
+    } else {
+        console.log("Unknown playback type");
     }
 }
 
 function stopMediaPlayer(player) {
-    switch(streamtype) {
-        case StreamType.DASH:
-            dashjs_stopDashMediaPlayer(player);
-            break;
-        
-        case StreamType.HLS:
-            hlsjs_stopMediaPlayer(player);
-            break;
+    if(playbacktype === PlaybackType.JSLIB) {
+        switch(streamtype) {
+            case StreamType.DASH:
+                dashjs_stopDashMediaPlayer(player);
+                break;
+            
+            case StreamType.HLS:
+                hlsjs_stopMediaPlayer(player);
+                break;
 
-        case StreamType.VE:
-            ve_stopMediaPlayer(player);
-            break;
-
-        default:
-            break;
+            default:
+                break;
+        }
+    } else if(playbacktype === PlaybackType.VE) {
+        ve_stopMediaPlayer(player);
+    } else {
+        console.log("Unknown playback type");
     }
 }
 
@@ -255,7 +281,8 @@ function dashjs_localavinfocallback(player, e) {
         avinfo.text = out;
     }
 
-    let text = (avinfo.video !== undefined ? avinfo.video  + "<br>" : "") +
+    let text = "<b>MPEG-DASH / dash.js</b><br>" +
+               (avinfo.video !== undefined ? avinfo.video  + "<br>" : "") +
                (avinfo.audio !== undefined ? avinfo.audio + "<br>" : "") +
                (avinfo.text !== undefined ? avinfo.text : "");
     if(text.length === 0) {
@@ -284,9 +311,9 @@ function hlsjs_initMediaPlayer() {
             console.log(
                 'manifest loaded, found ' + data.levels.length + ' quality level'
             );
-            console.log(data);
+            //console.log(data);
 
-            setAVInfo("Found " + data.levels.length + " quality levels");
+            setAVInfo("<b>HLS / hls.js</b><br>Found " + data.levels.length + " quality levels");
         });
     }
 
@@ -314,7 +341,13 @@ function ve_initMediaPlayer() {
 }
 
 function ve_startMediaPlayer(player, videoelement, url, servicetype, ttmlrenderingdiv = undefined) {
-    setAVInfo("Using HTML Video element for playback");
+    let text = "<b>";
+    if(streamtype === StreamType.DASH) {text += "MPEG-DASH"}
+    else if(streamtype === StreamType.HLS) {text += "HLS"}
+    else {text += "unknown"}
+    text += " / video element</b><br>";
+    text += "Using HTML Video element for playback";
+    setAVInfo(text);
     videoelement.src = url;
     videoelement.play();
 }
@@ -331,7 +364,63 @@ function queryURLParameter(query) {
     return queryDict[query];
 }
 
+function queryNumberOfURLParameters(url) {
+    let params = new URL(url).searchParams;
+    let i = 0;
+    for(const x of params) {i++;}
+    console.log("Number of URL parameters: " + i);
+    return i;
+}
+
 function setAVInfo(text) {
     let ele = document.getElementById("avinfo");
     ele.innerHTML = text;
+}
+
+function swapPlaybackType() {
+    let currenturl = window.location.href;
+    console.log("Current URL is: " + currenturl);
+    // Strip out the 'p' parameter
+    let cleanurl = removeParam('p', currenturl);
+    console.log("Clean URL is: " + cleanurl);
+    let newpparam;
+    if(playbacktype === PlaybackType.JSLIB) {
+        newpparam = "p=VE";
+    } else if(playbacktype === PlaybackType.VE) {
+        newpparam = "p=JSLIB";
+    } else {
+        console.error("Unknown playback type");
+    }
+    // Check whether the current URL has parameters
+    // If not, we need to add "?" and the parameters
+    // If it does, we need to add "&" and the parameters
+    let newurl;
+    if(queryNumberOfURLParameters(cleanurl) > 0) {
+        newurl = cleanurl + "&" + newpparam;
+    } else {
+        newurl = cleanurl + "?" + newpparam;
+    }
+    console.log("New URL is: " + newurl);
+
+    // Load the new URL
+    window.location.href = newurl;
+}
+
+// Borrowed from https://stackoverflow.com/questions/16941104/remove-a-parameter-to-the-url-with-javascript
+function removeParam(key, sourceURL) {
+    var rtn = sourceURL.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === key) {
+                params_arr.splice(i, 1);
+            }
+        }
+        if (params_arr.length) rtn = rtn + "?" + params_arr.join("&");
+    }
+    return rtn;
 }
